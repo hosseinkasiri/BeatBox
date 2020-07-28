263faa5b490b9097c82e5f20d09ea5b413d3ddf6
hosseinkasiri <hosseinkasiri5@gmail.com>
2020-07-28 17:46:19 +0430

    create recyclerView

--- app/src/main/java/com/example/beatbox/controller/BeatBoxFragment.java
+++ app/src/main/java/com/example/beatbox/controller/BeatBoxFragment.java
@@ -2,13 +2,20 @@ package com.example.beatbox.controller;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.beatbox.R;
+import com.example.beatbox.controller.model.Sound;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +23,10 @@ import com.example.beatbox.R;
  * create an instance of this fragment.
  */
 public class BeatBoxFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private SoundAdapter mAdapter;
+    private BeatBox mBeatBox;
 
     public BeatBoxFragment() {
         // Required empty public constructor
@@ -32,12 +43,62 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beat_box, container, false);
+        View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
+        mRecyclerView = view.findViewById(R.id.beat_box_recyclerview);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        mAdapter = new SoundAdapter(mBeatBox.getSounds());
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+
+        private Button mButton;
+        private Sound mSound;
+
+        public SoundHolder(@NonNull View itemView) {
+            super(itemView);
+            mButton = itemView.findViewById(R.id.list_item_sound_button);
+
+        }
+        public void bind(Sound sound){
+            mSound = sound;
+            mButton.setText(sound.getName());
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.list_item_sound,parent);
+            return new SoundHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder holder, int position) {
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSounds.size();
+        }
     }
 }
